173cf7542d18dc6bac46336be91ae3afe6d5bf01
Miguel <miguelmenendez2002@gmail.com>
2022-02-24 08:41:50 +0100

    models

--- models/models.py
+++ models/models.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
-
 class opencinema(models.Model):
-    _name = 'opencinema.opencinema'
-    _description = 'opencinema.opencinema'
+    _name = 'opencinema.film'
+    _description = 'OpenCinema Films'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
-    @api.depends('value')
-    def _value_pc(self):
-        for record in self:
-            record.value2 = float(record.value) / 100
